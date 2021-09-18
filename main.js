1d05ee67b3d7b8e9bd974fc2dce4934730dd87d6
Mehluli Hikwa <mehlulihikwa@gmail.com>
2021-09-18 22:15:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -14,23 +14,24 @@ if (setupEvents.handleSquirrelEvent()) {
    return;
 }
 
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-const {ipcMain} = require('electron')
-var path = require('path')
+// const electron = require('electron')
+// // Module to control application life.
+// const app = electron.app
+// const {ipcMain} = require('electron')
+// var path = require('path')
 
 
 
 
-const { app, BrowserWindow, Menu} = require('electron')
+// Module to create native browser window.
+const { theapp, BrowserWindow, Menu} = require('electron')
 
 const shell = require('electron').shell
 
-let win
+let windowForBrowser
 
 function createWindow() {
-    win = new BrowserWindow({
+    windowForBrowser = new BrowserWindow({
         width: 900,
         height: 700,
         webPreferences: {
@@ -38,10 +39,10 @@ function createWindow() {
         }
     })
 
-    win.loadFile('index.html')
+    windowForBrowser.loadFile('index.html')
     
-    win.on('closed', () => {
-        win = null
+    windowForBrowser.on('closed', () => {
+        windowForBrowser = null
     })
 
     var menu = Menu.buildFromTemplate([
@@ -58,7 +59,7 @@ function createWindow() {
                 {
                     label:'Exit', 
                     click() { 
-                        app.quit() 
+                        theapp.quit() 
                     } 
                 }
             ]
@@ -68,19 +69,19 @@ function createWindow() {
 }
 
 // This method will be called when Electron has finished initialization and is ready to create browser windows. Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+theapp.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+theapp.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-        app.quit()
+        theapp.quit()
     }
 })
 
-app.on('activate', () => {
+theapp.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the dock icon is clicked and there are no other windows open.
-    if (win === null) {
+    if (windowForBrowser === null) {
         createWindow()
     }
 })
