f9814f8929546ceedc71715e170fcf3e695fa9d9
Mehluli Hikwa <mehlulihikwa@gmail.com>
2023-09-22 03:04:34 +0200

    Add Electron to project

--- main.js
+++ main.js
@@ -1,31 +1,25 @@
 const { app, BrowserWindow } = require('electron')
-const path = require('path')
 
-function createMainWindow() {
-	const mainWindow = new BrowserWindow({
-		title: 'Image Compressor',
+function createWindow() {
+	const win = new BrowserWindow({
 		width: 800,
 		height: 600,
 		webPreferences: {
-			preload: path.join(__dirname, 'preload.js')
+			nodeIntegration: true
 		}
 	})
 
-	mainWindow.loadFile('index.html')
+	win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-	createMainWindow()
+	createWindow()
 
-	app.on('activate', () => {
-		if (BrowserWindow.getAllWindows().length === 0) {
-			createMainWindow()
-		}
+	app.on('activate', function () {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow()
 	})
 })
 
-app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit()
-	}
+app.on('window-all-closed', function () {
+	if (process.platform !== 'darwin') app.quit()
 })
