b2c783c378df0675804b627450085d49c25a8d48
Mehluli Hikwa <mehlulihikwa@gmail.com>
2025-08-21 10:01:31 +0200

    Create a robust Offline/Cache strategy

--- next.config.js
+++ next.config.js
@@ -5,6 +5,37 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: ({ request }) => request.mode === 'navigate',
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'pages',
+        expiration: {
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        },
+      },
+    },
+    {
+      urlPattern: /\.(?:js|css)$/,
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-resources',
+      },
+    },
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg|gif|woff2)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images-and-fonts',
+        expiration: {
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        },
+      },
+    },
+  ],
 });
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
